5ebf27d39237fa0824c73f7aef8066b047125627
이진헌 <6219jhl@naver.com>
2022-12-04 00:12:47 +0900

    레벨 추가 그외 많은거

---
+++ raw_file/stage1.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="stage1" tilewidth="64" tileheight="48" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="9">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/checkpoint.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="48" source="../../ukkagame/graphics/tile/cloud.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/flower.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/goal.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/ground.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/sky.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/spike.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../ukkagame/graphics/tile/topground.png"/>
+ </tile>
+ <tile id="17">
+  <image width="25" height="25" source="../../ukkagame/graphics/tile/player.png"/>
+ </tile>
+ <tile id="18">
+  <image width="25" height="25" source="../../ukkagame/graphics/tile/enemy_detect.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../graphics/tile/enemy/penguin1.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../graphics/tile/block.png"/>
+ </tile>
+ <tile id="22">
+  <image width="25" height="25" source="../graphics/tile/hidden.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../graphics/tile/mysteryblock.png"/>
+ </tile>
+</tileset>
